133bfa8b99ccb975559232313655f3b3282d41bf
tjanuki <t.januki@gmail.com>
2024-12-17 16:22:05 -0500

    Add Gates

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController
+{
+    public function index()
+    {
+        return User::get();
+    }
+
+    public function show($id)
+    {
+        return User::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        return User::create($request->all());
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+        $user->update($request->all());
+        return $user;
+    }
+
+    public function destroy($id)
+    {
+        return User::destroy($id);
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        Gate::define('admin-access', function (User $user) {
+            return $user->is_admin;
+        });
+
+        Gate::define('manage-article', function (User $user, $article) {
+            return Gate::allows('admin-access') || $user->id === $article->user_id;
+        });
     }
 
     /**
